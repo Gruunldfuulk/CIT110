9fc711a094710530aefe1cefe87d2de689381557
Alex Juhasz <gruunldfuulk@gmail.com>
2018-12-09 17:07:58 -0500

    Version4

--- RpgGame/Program.cs
+++ RpgGame/Program.cs
@@ -8,6 +8,15 @@ namespace RpgGame
 {
     static class Program
     {
+        //***********************************
+        //Title: RPG Game (Dungeon)
+        //Application Type: WinForms
+        //Description: (describe the purpose and function)
+        //Author: Alexandru Juhasz
+        //Date Created: 11/30/2018
+        //Last Modified: 12/09/2018
+        //************************************
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
